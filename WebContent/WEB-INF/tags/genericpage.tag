2a0def8256eee46aecdbbff54a372ab808cdac8f
David SALMON <David SALMON>
2016-08-25 13:32:44 +0200

    tchat global

--- WebContent/WEB-INF/tags/genericpage.tag
+++ WebContent/WEB-INF/tags/genericpage.tag
@@ -27,7 +27,50 @@
 			</div>
 		</div>
 		<div id="content">
-			<jsp:doBody/>
+			<jsp:doBody />
 		</div>
-	</body>
+		<div id="chat">
+			<div id="ChatContent"
+		style="width: 500px; height: 200px; border: 1px solid black; overflow-y: scroll;">
+		<ul class="collection" id="chatMessageContainer">
+		</ul>
+	</div>
+<div class="input-field col s6"  style="width:500px;">
+    <input id="messageInput" style="width:500px;" type="text" class="validate" placeholder="Entrez votre message...">
+</div>
+</body>
+<script type="text/javascript">
+	var connectionChat = new WebSocket("ws://" + location.host
+			+ "/MediaStripe-web/testSocket");
+	document.getElementById("messageInput").addEventListener("keydown",
+			function(e) {
+				if (e.keyCode === 13 && this.value !== "") {
+					connectionChat.send(this.value);
+					this.value = "";
+				}
+			});
+	connectionChat.onmessage = function(event) {
+		var li = document.createElement("li");
+        li.className= "collection-item avatar";
+		//li.innerHTML = event.data;
+        /*var img = document.createElement('img');
+        img.className = "circle";
+        img.setAttribute("src", "images/yuna.jpg");*/
+        var span = document.createElement("span");
+        span.innerHTML= event.data;
+        if(event.data==='Vous devez vous connecter pour pouvoir utiliser le chat')
+        	span.style.color="red";
+        //li.appendChild(img);
+        li.appendChild(span);
+
+		//li.setAttribute("style","color: #7C6F6F; font-size: 70%;margin: 5px;");
+		document.getElementById("chatMessageContainer").appendChild(li);
+        document.getElementById("ChatContent").scrollTop = document.getElementById("ChatContent").scrollHeight;
+        //objDiv.scrollTop = objDiv.scrollHeight;
+	}
+</script>
+		</div>
+	
+
+</body>
 </html>
--- src/main/java/com/imie/websockets/ChatEndPoint.java
+++ src/main/java/com/imie/websockets/ChatEndPoint.java
@@ -4,38 +4,49 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.servlet.http.HttpSession;
+import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint(value = "/testSocket")
+import com.imie.entities.Utilisateur;
+import com.sun.net.httpserver.HttpsConfigurator;
+
+@ServerEndpoint(value = "/testSocket",configurator = GetHttpSessionConfigurator.class)
 public class ChatEndPoint {
 
 	private Map<String,ChatUser> mapChatUsers;
 	private static CopyOnWriteArrayList<Session> userSessions;
 	
+
+	
 	@OnOpen
-	public void onOpen(Session session){
-		System.out.println("nouvelle connection sur la websocket");
+	public void onOpen(Session session, EndpointConfig config){
+		HttpSession userSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+		System.out.println(userSession.getAttribute("utilisateur"));
+		if(userSession.getAttribute("utilisateur")!= null){
+			Utilisateur user = (Utilisateur)userSession.getAttribute("utilisateur");
+			session.getUserProperties().put("pseudo", user.getPrenom()+" "+user.getNom());
+		}
+		
 		if(this.userSessions == null){
 			userSessions = new CopyOnWriteArrayList<Session>();
 			
 		}
 		userSessions.add(session);
-		System.out.println("Personne connecté :" + userSessions.size());
+		System.out.println("Utilisateurs connectés :" + userSessions.size());
 	}
 	
 	@OnMessage
 	public void handleMessage(String message,Session s) throws IOException{
+		System.out.println(s.getUserProperties().get("utilisateur"));
 		if(!s.getUserProperties().containsKey("pseudo")){
-			s.getUserProperties().put("pseudo", message);
 			for(Session session:userSessions){
 				if(s.equals(session)){
-					session.getBasicRemote().sendText("Vous êtes maintenant connecté(e) en tant que " +message+".");
-				}else{
-					session.getBasicRemote().sendText(message +" a rejoint le tchat.");
+					session.getBasicRemote().sendText("Vous devez vous connecter pour pouvoir utiliser le chat");
 				}
 			}
 		}else{
@@ -51,7 +62,7 @@ public class ChatEndPoint {
 	public void removeSession(Session s){
 		String pseudo = (String)s.getUserProperties().get("pseudo");
 		userSessions.remove(s);
-		for(Session session : userSessions){
+		/*for(Session session : userSessions){
 				try {
 					session.getBasicRemote().sendText(pseudo+" a quitté le tchat");
 				} catch (IOException e) {
@@ -59,7 +70,7 @@ public class ChatEndPoint {
 					e.printStackTrace();
 				}
 			
-		}
+		}*/
 		
 	}
 }
